c8c4a622fd76c02e1fa3817fb020b467f82b947c
Skgland <bb-github@t-online.de>
2021-12-02 21:37:47 +0100

    day 1

---
+++ src/bin/day1-2.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let result = aoc2021::day1::part2(include_str!("../../input/day1.txt"));
+    println!("{}", result)
+}
---
+++ src/day1.rs
@@ -0,0 +1,58 @@
+fn parse_input(input: &str) -> impl Iterator<Item = u32> + '_ {
+    input.lines().map(|line| line.parse().unwrap())
+}
+
+///
+///```rust
+/// # use aoc2021::day1::part1;
+/// let input = include_str!("../input/day1.example.txt");
+///
+/// assert_eq!(part1(input), 7);
+/// ```
+///
+pub fn part1(input: &str) -> u32 {
+    let mut iter = parse_input(input);
+
+    let mut last = iter.next().unwrap();
+
+    let mut counter = 0;
+    for current in iter {
+        if current > last {
+            counter += 1;
+        }
+        last = current
+    }
+
+    return counter;
+}
+
+///
+///```rust
+/// # use aoc2021::day1::part2;
+/// let input = include_str!("../input/day1.example.txt");
+/// assert_eq!(part2(input), 5);
+/// ```
+///
+pub fn part2(input: &str) -> u32 {
+    let mut iter = parse_input(input);
+
+    let mut window1 = iter.next().unwrap();
+    let mut window2 = iter.next().unwrap();
+    let mut window3 = iter.next().unwrap();
+
+    window2 += window3;
+    window1 += window2;
+
+    let mut counter = 0;
+
+    for current in iter {
+        if window2 + current > window1 {
+            counter += 1;
+        }
+        window1 = window2 + current;
+        window2 = window3 + current;
+        window3 = current;
+    }
+
+    return counter;
+}
